a00663a0a89a811daa1ac772b5e7a69a230eb71c
James Daly <james.daly@parexel.com>
2025-10-13 15:13:19 -0700

    build: use nitro + bun

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
   plugins: [
-    // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ["./tsconfig.json"],
     }),
     tanstackStart({}),
+    nitro(),
     viteReact(),
   ],
 });
